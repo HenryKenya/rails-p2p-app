3bdc191f0c29fb27b23d6b463015626b38078470
Henry <henry.onyango@outlook.com>
2018-04-27 21:44:13 +0300

    Change login route to simply login

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # root path
   root to: 'pages#index'
+
+  # change devise routes
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
 end
