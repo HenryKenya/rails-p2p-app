adee66869da8394acda03f91bdda5134866eece9
Henry <henry.onyango@outlook.com>
2018-04-28 11:08:27 +0300

    Transaction table migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :transactions, foreign_key: :sender_id, class_name 'Transaction'
+  has_many :transactions, foreign_key: :sender_id, class_name: 'Transaction'
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- db/migrate/20180428075155_create_transactions.rb
+++ db/migrate/20180428075155_create_transactions.rb
@@ -1,7 +1,9 @@
 class CreateTransactions < ActiveRecord::Migration[5.1]
   def change
     create_table :transactions do |t|
-
+      t.integer :amount
+      t.references :user, foreign_key: true
+      t.belongs_to :category, index: true
       t.timestamps
     end
   end
