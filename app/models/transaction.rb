eb520f9080e688243497a5479a77d32ebda6f7d7
Henry <henry.onyango@outlook.com>
2018-04-30 21:48:21 +0300

    added index to schema

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ApplicationRecord
+    belongs_to :category, foreign_key: :category_id, class_name: 'Category'
     belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
-    belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
-    belongs_to :category
+    belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'   
 end
--- db/migrate/20180428075155_create_transactions.rb
+++ db/migrate/20180428075155_create_transactions.rb
@@ -3,8 +3,14 @@ class CreateTransactions < ActiveRecord::Migration[5.1]
     create_table :transactions do |t|
       t.integer :amount
       t.references :user
-      t.integer :category_id
+      t.references :category_id
       t.timestamps
     end
+
+    add_index :transactions, :recipient_id
+    add_index :transactions, :sender_id
+    add_index :transactions, [:recipient_id, :sender_id], unique: true
+    add_index :transactions, :category_id, unique: true
+
   end
 end
