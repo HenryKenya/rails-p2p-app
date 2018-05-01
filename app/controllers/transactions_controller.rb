a72886b322bbcf1b05a8499ade34abf97461f7c9
Henry <henry.onyango@outlook.com>
2018-05-01 10:53:07 +0300

    Added balance to user model. Users can perfom basic transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,16 +1,25 @@
 class TransactionsController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_variables
+    
+
     def new
         @transaction = Transaction.new
-        @categories = Category.all
-        @users = User.all
-        @id = User.find_by_id(current_user).id
+        if @sender_balance.nil?
+            @balance = 0
+        end
     end
 
     def create
         
         @transaction = Transaction.new(transaction_params)
         if @transaction.save
+            sender = User.find(@transaction.sender_id)
+            recipient = User.find(@transaction.recipient_id)
+
+            sender.update_attributes(balance: @sender_balance - @transaction.amount)
+            recipient.update_attributes(balance: recipient.balance + @transaction.amount)
+
             flash[:success] = "Transaction successful!"
             redirect_to new_transaction_path
         else
@@ -23,4 +32,13 @@ class TransactionsController < ApplicationController
         def transaction_params
             params.require(:transaction).permit(:amount, :category_id, :sender_id, :recipient_id)
         end
+
+        def set_variables
+            @categories = Category.all #call all categories for display in select
+            @users = User.all # call all users for display in select
+            @sender_id =User.find_by_id(current_user).id
+            @sender_balance = User.find_by_id(current_user).balance
+        end
+
+        
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,13 @@ class Transaction < ApplicationRecord
   belongs_to :category
   belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
   belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
+
+  # before_save :check_and_update_balance
+
+  # def check_and_update_balance
+  #   recipient = User.find(recipient_id)
+  #   sender = User.find(sender_id)
+  #   sender.update_attributes(balance: 1000)
+  #   recipient.update_attributes(balance: 20000)
+  # end
 end
