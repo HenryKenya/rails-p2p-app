0a4d2ca68748ac2883fed8f4ee96d9054281f343
Henry <henry.onyango@outlook.com>
2018-05-01 12:21:51 +0300

    update attributes

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -16,9 +16,9 @@ class TransactionsController < ApplicationController
         @transaction = Transaction.new(transaction_params)
         sender = User.find(@transaction.sender_id)
         recipient = User.find(@transaction.recipient_id)
-        
+        test = @sender_balance - @transaction.amount
         if @transaction.save
-            sender.update_attributes(balance: @sender_balance - @transaction.amount)
+            sender.update_attributes(balance: test)
             flash[:success] = "Transaction successful!"
             redirect_to new_transaction_path
         else
