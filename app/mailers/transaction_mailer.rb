efd844d999206d34e7bbcbab6f38ae53aac34865
Henry <henry.onyango@outlook.com>
2018-05-01 16:50:06 +0300

    fixing nil class for transaction amount

--- app/mailers/transaction_mailer.rb
+++ app/mailers/transaction_mailer.rb
@@ -2,7 +2,7 @@ class TransactionMailer < ApplicationMailer
   default :from => "noreply@zeecash2.herokuapp.com"
   def transaction_confirmation(recipient, sender)
     @recipient = recipient.email
-    @transaction = recipient.transactions.last.amount ||= 0
+    @transaction = recipient.transactions.last
     @sender = sender.email
     mail(:to => "<#{recipient.email}>", :subject => "New Transaction")
   end
